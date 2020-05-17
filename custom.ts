2308764bdd5cc4eb77184683ede2ea5b962b22b4
Robert Thomitzek <thro@thomitzek.com>
2020-05-17 20:49:34 +0200

    First Save

--- custom.ts
+++ custom.ts
@@ -1,1 +1,21 @@
 // Gib deinen Code hier ein
+//% color="#008272"weight=100 icon="\uf11b" block="M5 Joystick"
+namespace m5joystick {
+//% block="M5 Joysick %i2cMotor|run %n"
+  export function SickVal() : joyStickVal {
+    let retVal: joyStickVal;
+    let buf = pins.createBuffer(3);
+    buf = pins.i2cReadBuffer(0x52, 3);
+    retVal.valX = buf[0];
+    retVal.valY = buf[1];
+    retVal.button = buf[2];
+    return retVal;
+  }
+}
+
+//% blockNamespace=m5joystick
+class joyStickVal {
+   valX: number;
+   valY: number;
+   button: number;
+}
