99e9b2cab830c253ddaa0f4efc07c7101c68f99e
Robert Thomitzek <thro@thomitzek.com>
2020-05-17 21:07:42 +0200

    Aktualisiere main.blocks, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,6 +1,13 @@
 // Gib deinen Code hier ein
 //% color="#008272"weight=100 icon="\uf11b" block="M5 Joystick"
 namespace m5joystick {
+
+export enum ValSelect {
+    X = 1,
+    Y = 2,
+    Button = 3,
+}
+
 export class joyStickVal {
    public valX: number;
    public valY: number;
@@ -16,4 +23,22 @@ export class joyStickVal {
     retVal.button = buf[2];
     return retVal;
   }
+//% block="M5 Joysick %joyStickVal Werte"
+  export function axisVal(valSel: ValSelect ) : number {
+    let retVal: number;
+    let buf = pins.createBuffer(3);
+    buf = pins.i2cReadBuffer(0x52, 3);
+    switch(valSel) {
+        case ValSelect.X: {
+            retVal = buf[0];
+        }
+        case ValSelect.Y: {
+            retVal = buf[1];
+        }
+        case ValSelect.Button: {
+            retVal = buf[2];
+        }
+    return retVal;
+  }
+
 }
